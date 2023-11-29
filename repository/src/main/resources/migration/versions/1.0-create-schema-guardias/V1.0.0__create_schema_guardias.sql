0a08cf0f8035dc2da8fecd67dc355f729857614e
Quique Sancho <ensancho@mercadona.es>
2023-11-29 15:20:45 +0100

    feat: Finalize update XML File and add BBDD postgreSQL with docker

--- repository/src/main/resources/migration/versions/1.0-create-schema-guardias/V1.0.0__create_schema_guardias.sql
+++ repository/src/main/resources/migration/versions/1.0-create-schema-guardias/V1.0.0__create_schema_guardias.sql
@@ -1,5 +1,7 @@
-CREATE SEQUENCE teaching_hours_id_seq START WITH 1 INCREMENT BY 1 NO CYCLE;
+DROP SEQUENCE IF EXISTS teaching_hours_id_seq;
+DROP SEQUENCE IF EXISTS timetable_group_id_seq;
 
+CREATE SEQUENCE teaching_hours_id_seq START WITH 1 INCREMENT BY 1 NO CYCLE;
 CREATE SEQUENCE timetable_group_id_seq START WITH 1 INCREMENT BY 1 NO CYCLE;
 
 DROP TABLE IF EXISTS ABSENCE_TEACHER CASCADE;
@@ -7,7 +9,9 @@ DROP TABLE IF EXISTS REGISTRY_ABSENCE CASCADE;
 DROP TABLE IF EXISTS TEACHERS CASCADE;
 DROP TABLE IF EXISTS TEACHING_HOURS CASCADE;
 DROP TABLE IF EXISTS TIMETABLE_GROUP CASCADE;
-
+DROP TABLE IF EXISTS TEACHERS_AUD CASCADE;
+DROP TABLE IF EXISTS TEACHING_HOURS_AUD CASCADE;
+DROP TABLE IF EXISTS TIMETABLE_GROUP_AUD CASCADE;
 
 /*==============================================================*/
 /* Table: TEACHERS                                              */
@@ -18,10 +22,10 @@ CREATE TABLE teachers (
     name VARCHAR(255),
     first_surname VARCHAR(255),
     second_surname VARCHAR(255),
-    cod_user_creation VARCHAR(255) DEFAULT 'Guardias-back-app' NOT NULL,
+/*    cod_user_creation VARCHAR(255) DEFAULT 'Guardias-back-app' NOT NULL,
     creation_date TIMESTAMP NOT NULL,
     cod_user_modification VARCHAR(255),
-    modification_date TIMESTAMP,
+    modification_date TIMESTAMP,*/
     PRIMARY KEY (teacher_id)
 );
 
@@ -29,16 +33,16 @@ CREATE TABLE teachers (
 /* Table: TEACHING_HOURS                                        */
 /*==============================================================*/
 CREATE TABLE teaching_hours (
-    teaching_hours_id BIGINT DEFAULT teaching_hours_id_seq.nextval,
+    teaching_hours_id BIGINT DEFAULT nextval('teaching_hours_id_seq'::regclass),
     start_hour TIME,
     end_hour TIME,
     day_of_week VARCHAR(255),
     teacher_id VARCHAR(9),
     occupation VARCHAR(255),
-    cod_user_creation VARCHAR(255) DEFAULT 'Guardias-back-app' NOT NULL,
+/*    cod_user_creation VARCHAR(255) DEFAULT 'Guardias-back-app' NOT NULL,
     creation_date TIMESTAMP NOT NULL,
     cod_user_modification VARCHAR(255),
-    modification_date TIMESTAMP,
+    modification_date TIMESTAMP,*/
     PRIMARY KEY (teaching_hours_id),
     FOREIGN KEY (teacher_id) REFERENCES teachers (teacher_id)
 );
@@ -47,7 +51,7 @@ CREATE TABLE teaching_hours (
 /* Table: TIMETABLE_GROUP                                       */
 /*==============================================================*/
 CREATE TABLE timetable_group (
-    timetable_group_id BIGINT DEFAULT timetable_group_id_seq.nextval,
+    timetable_group_id BIGINT DEFAULT nextval('timetable_group_id_seq'::regclass),
     day_of_week VARCHAR(255),
     start_hour TIME,
     end_hour TIME,
@@ -55,10 +59,64 @@ CREATE TABLE timetable_group (
     classroom VARCHAR(255),
     content VARCHAR(255),
     teacher_id VARCHAR(9),
-    cod_user_creation VARCHAR(255) NOT NULL DEFAULT 'Guardias-back-app',
+/*    cod_user_creation VARCHAR(255) NOT NULL DEFAULT 'Guardias-back-app',
     creation_date TIMESTAMP NOT NULL,
     cod_user_modification VARCHAR(255),
-    modification_date TIMESTAMP,
+    modification_date TIMESTAMP,*/
+    PRIMARY KEY (timetable_group_id),
+    FOREIGN KEY (teacher_id) REFERENCES teachers (teacher_id)
+);
+
+/*==============================================================*/
+/* Table: TEACHERS_AUD                                          */
+/*==============================================================*/
+CREATE TABLE teachers_aud (
+    teacher_id VARCHAR(9) NOT NULL,
+    email VARCHAR(255),
+    name VARCHAR(255),
+    first_surname VARCHAR(255),
+    second_surname VARCHAR(255),
+/*    cod_user_creation VARCHAR(255) NOT NULL,
+    creation_date TIMESTAMP NOT NULL,
+    cod_user_modification VARCHAR(255),
+    modification_date TIMESTAMP,*/
+    PRIMARY KEY (teacher_id)
+);
+
+/*==============================================================*/
+/* Table: TEACHING_HOURS_AUD                                    */
+/*==============================================================*/
+CREATE TABLE teaching_hours_aud (
+    teaching_hours_id BIGINT NOT NULL,
+    start_hour TIME,
+    end_hour TIME,
+    day_of_week VARCHAR(255),
+    teacher_id VARCHAR(9),
+    occupation VARCHAR(255),
+/*    cod_user_creation VARCHAR(255) NOT NULL,
+    creation_date TIMESTAMP NOT NULL,
+    cod_user_modification VARCHAR(255),
+    modification_date TIMESTAMP,*/
+    PRIMARY KEY (teaching_hours_id),
+    FOREIGN KEY (teacher_id) REFERENCES teachers (teacher_id)
+);
+
+/*==============================================================*/
+/* Table: TIMETABLE_GROUP_AUD                                   */
+/*==============================================================*/
+CREATE TABLE timetable_group_aud (
+    timetable_group_id BIGINT NOT NULL,
+    day_of_week VARCHAR(255),
+    start_hour TIME,
+    end_hour TIME,
+    group_id VARCHAR(255),
+    classroom VARCHAR(255),
+    content VARCHAR(255),
+    teacher_id VARCHAR(9),
+/*    cod_user_creation VARCHAR(255) NOT NULL,
+    creation_date TIMESTAMP NOT NULL,
+    cod_user_modification VARCHAR(255),
+    modification_date TIMESTAMP,*/
     PRIMARY KEY (timetable_group_id),
     FOREIGN KEY (teacher_id) REFERENCES teachers (teacher_id)
 );
