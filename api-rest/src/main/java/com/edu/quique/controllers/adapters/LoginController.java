0ed9d25ad8f75317729e2279297d49b899e48712
Quique Sancho <ensancho@mercadona.es>
2024-01-10 17:09:44 +0100

    feat: Add Login function

--- api-rest/src/main/java/com/edu/quique/controllers/adapters/LoginController.java
+++ api-rest/src/main/java/com/edu/quique/controllers/adapters/LoginController.java
@@ -1,8 +1,30 @@
 package com.edu.quique.controllers.adapters;
 
+import com.edu.quique.api.LoginApi;
+import com.edu.quique.api.model.LoginRequest;
+import com.edu.quique.api.model.LoginResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-public class LoginController {}
+public class LoginController implements LoginApi {
+  private final AuthenticationManager authenticacionManager;
+
+  @Override
+  public ResponseEntity<LoginResponse> login(LoginRequest loginRequest) {
+    Authentication authentication =
+        authenticacionManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                loginRequest.getUsername(), loginRequest.getPassword()));
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+    var res = new LoginResponse();
+    res.setToken("Login check!!");
+    return ResponseEntity.ok(res);
+  }
+}
--- boot/src/main/java/com/edu/quique/WebSecurityConfig.java
+++ boot/src/main/java/com/edu/quique/WebSecurityConfig.java
@@ -1,4 +1,3 @@
-/*
 package com.edu.quique;
 
 import org.springframework.context.annotation.Bean;
@@ -9,53 +8,40 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-        .csrf().disable()
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf()
+        .disable()
         .authorizeRequests()
-        .antMatchers("/api/login/**").permitAll()
+        .antMatchers("/login/**")
+        .permitAll()
         .anyRequest()
         .authenticated()
         .and()
         .httpBasic();
-	}
+  }
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.ldapAuthentication()
+        .userSearchFilter("(uid={0})")
+        .userSearchBase("ou=Users,dc=ieslavereda,dc=local")
+        .groupSearchFilter("cn={0}")
+        .groupSearchBase("ou=Users,dc=ieslavereda,dc=local")
+        .contextSource()
+        .url("ldap://localhost:389")
+        .managerDn("cn=admin,dc=ieslavereda,dc=local")
+        .managerPassword("123456789");
+  }
 
-//		auth.ldapAuthentication()
-//        .userSearchFilter("(uid={0})")
-//        .userSearchBase("dc=ieslavereda,dc=local")
-//        .groupSearchBase("ou=Users,dc=ieslavereda,dc=local")
-//        .groupSearchFilter("cn={0}")
-//        .contextSource()
-//        .url("ldap://192.168.1.143")
-//        .port(389)
-//        .managerDn("cn=admin,dc=ieslavereda,dc=local")
-//        .managerPassword("12345678");
-		auth.authenticationProvider(activeDirectoryLdapAuthenticationProvider());
-    }
-
-	@Bean
-	public ActiveDirectoryLdapAuthenticationProvider activeDirectoryLdapAuthenticationProvider() {
-		ActiveDirectoryLdapAuthenticationProvider provider =
-				new ActiveDirectoryLdapAuthenticationProvider("ieslavereda.local", "ldap://localhost:389");
-		provider.setConvertSubErrorCodesToExceptions(true);
-		provider.setUseAuthenticationRequestCredentials(true);
-		return provider;
-	}
-
-	@Override
-	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception  {
-		return super.authenticationManagerBean();
-	}
-
-}*/
+  @Override
+  @Bean
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+}
