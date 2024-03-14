28da09b5d299d5ea2febe2007a6c4a8ae72d871d
ensancho <ensancho@mercadona.es>
2024-03-14 12:27:11 +0100

    feat: Replace with lambda expression

--- repository-ldap/src/main/java/com/edu/quique/repository/adapter/LdapRepositoryAdapter.java
+++ repository-ldap/src/main/java/com/edu/quique/repository/adapter/LdapRepositoryAdapter.java
@@ -6,21 +6,17 @@ import org.springframework.ldap.core.AttributesMapper;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.stereotype.Service;
 
-import javax.naming.NamingException;
-import javax.naming.directory.Attributes;
 import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class LdapRepositoryAdapter implements LdapRepositoryPort {
-    private LdapTemplate ldapTemplate;
+  private LdapTemplate ldapTemplate;
 
-    public List<String> getOUsByMail(String email) {
-        return ldapTemplate.search("", "(mail=" + email + ")", new AttributesMapper<String>() {
-            @Override
-            public String mapFromAttributes(Attributes attributes) throws NamingException {
-                return attributes.get("ou").get().toString();
-            }
-        });
-    }
+  public List<String> getOUsByMail(String email) {
+    return ldapTemplate.search(
+        "",
+        "(mail=" + email + ")",
+        (AttributesMapper<String>) attributes -> attributes.get("ou").get().toString());
+  }
 }
